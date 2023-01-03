d3efeeaa5c7210dd1d4ed6ca224cca1e2dcd0459
Faruk <adekoladamilola4@gmail.com>
2023-01-03 20:19:29 +0100

    Refix task 0 and 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 import random
+
+signed = True
 number = random.randint(-10000, 10000)
+if number < 0:
+    signed = False
+    number = -number
 last_digit = number % 10
+if not signed:
+    number = number * -1;
+    last_digit = last_digit * -1
 str = f"Last digit of {number} is {last_digit}"
 if last_digit > 5:
     print(str, "and is greater than 5")
