88fcfee447c9d3fc4d85d3af40d16749c02168fe
Faruk <adekoladamilola4@gmail.com>
2023-01-03 22:56:28 +0100

    Refix task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(00, 99):
-    print("{0:0<2d}".format(i), end=", ")
-print(99)
+for i in range(0, 100):
+    if not (i == 99):
+        print("{0:0<2d}".format(i), end=", ")
+print(i);
+
