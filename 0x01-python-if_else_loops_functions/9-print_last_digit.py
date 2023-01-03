3ee283c64a305c5dbf33835f52cacb9f312a476f
Faruk <adekoladamilola4@gmail.com>
2023-01-03 22:12:05 +0100

    Done with task 6,9,10,11 and 12

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+def print_last_digit(number):
+    signed = True
+    if number < 0:
+        signed = False
+        number = -number
+    last_digit = number % 10
+    if not signed:
+        last_digit = last_digit * -1
+    return last_digit
