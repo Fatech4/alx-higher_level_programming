c19c347f520b34d01b7128bd519687513baef5ac
Faruk <adekoladamilola4@gmail.com>
2023-01-05 23:58:50 +0100

    Refix task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     lis = dir("hidden_4")
     for i in lis:
-        print("{}".format(i))
+        if not (i[0] == "_" and i[1] == "_"):
+            print("{}".format(i))
