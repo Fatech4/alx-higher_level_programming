54a8ed840235a32769fcadfd42bcdee5d0b1052d
Faruk <adekoladamilola4@gmail.com>
2023-01-05 22:00:18 +0100

    Refix task 2 to conform to pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif (length - 1) == 1:
         print("1 argument:")
-        print("{}: {}".format(1, sys.argv[1]) )
+        print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(length-1))
         for item in range(1, len(sys.argv)):
