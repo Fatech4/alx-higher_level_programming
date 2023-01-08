40769212cba1b5c1bdc1e3164420d9f0f0c31f66
Faruk <adekoladamilola4@gmail.com>
2023-01-09 00:58:00 +0100

    Done with task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,21 +2,30 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    num1 = 0
-    num2 = 
+    num_ta1 = 0
+    num_ta2 = 0
+    num_tb1 = 0
+    num_tb2 = 0
     length1 = len(tuple_a)
     length2 = len(tuple_b)
-    if length1 == 0 and length2 == 0:
-        num1 = 0
-        num2 = 0
-    if length1 == 1 and length2 == 1:
-        num1 = tuple_a[0] + tuple_b[0]
-        num2 = 0
-    if length1 > 1 and :
-        num2 = 0 + tuple_b[0]
-    if length1 > 1:
-        num1 = tuple_a[0] + tuple_a[1]
-    if length2 > 1:
-        num2 = tuple_b[0] + tuple_b[1]
-    return (num1, num2)
-        
+
+    if length1 == 0:
+        num_ta1 = 0
+        num_ta2 = 0
+    elif length1 == 1:
+        num_ta1 = tuple_a[0]
+        num_ta2 = 0
+    else:
+        num_ta1 = tuple_a[0]
+        num_ta2 = tuple_a[1]
+    if length2 == 0:
+        num_tb1 = 0
+        num_tb2 = 0
+    elif length2 == 1:
+        num_tb1 = tuple_b[0]
+        num_tb2 = 0
+    else:
+        num_tb1 = tuple_b[0]
+        num_tb2 = tuple_b[1]
+
+    return (num_ta1 + num_tb1, num_ta2 + num_tb2) 
