50fae4e37b30c54db286cfaa6ade8f098114ee52
Faruk <adekoladamilola4@gmail.com>
2023-03-12 17:30:03 +0100

    Refix test case

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,7 @@
 class MyList(list):
     """ A class that inherit from list object and define a new instance method
     """
-    def __init__(self):
-        """ Initialize the constructor of the super class here """
-
-        super().__init__()
+    
 
     def print_sorted(self):
         """ New instance method of the subclass object """
