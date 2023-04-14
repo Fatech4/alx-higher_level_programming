e802383a654fbbd8471e5dff6b875f779962f5bb
Faruk <adekoladamilola4@gmail.com>
2023-04-14 22:43:19 +0100

    Refix task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@
 const arg = process.argv.splice(2);
 if (arg.length === 0) {
   console.log('No argument');
-} else {
+} else if (arg.length === 1) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
