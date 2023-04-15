301d51240ddfebb50a8e0669ea05b503498e16f0
Faruk <adekoladamilola4@gmail.com>
2023-04-15 22:39:24 +0100

    Done with task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const wide = [];
+    for (let i = 1; i <= this.width; i++) {
+      wide.push('X');
+    }
+    for (let j = 1; j <= this.height; j++) {
+      console.log(wide.join(''));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
