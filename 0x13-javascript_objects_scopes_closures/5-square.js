6b77519ebc6d76c88cce2604f6aead2145645da5
Faruk <adekoladamilola4@gmail.com>
2023-04-15 23:15:31 +0100

    Refix task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,8 +3,7 @@ const Rectangle = require('./4-rectangle.js');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
-    super();
-    this.size = size;
+    super(size, size);
   }
 
   print () {
