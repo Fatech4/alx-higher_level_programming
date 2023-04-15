adc21bee22c0fbb906b2e96c3f146934b664e136
Faruk <adekoladamilola4@gmail.com>
2023-04-15 23:10:18 +0100

    Refix

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,33 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    const wide = [];
-    for (let i = 1; i <= this.width; i++) {
-      wide.push('X');
-    }
-    for (let j = 1; j <= this.height; j++) {
-      console.log(wide.join(''));
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
+const Rectangle = require('./4-rectangle.js');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
