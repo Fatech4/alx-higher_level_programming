16aa55a1754c81c41d1065130f1d56376ec6d5aa
Faruk <adekoladamilola4@gmail.com>
2023-04-16 19:52:45 +0100

    refix task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,8 +5,8 @@ const file1 = arg[0];
 const file2 = arg[1];
 const destFile = arg[2];
 function concatFiles (file1, file2, destFile) {
-  const content1 = fs.readFile(file1);
-  const content2 = fs.readFile(file2);
-  fs.writeFile(destFile, content1 + content2);
+  const content1 = fs.readFileSync(file1);
+  const content2 = fs.readFileSync(file2);
+  fs.writeFileSync(destFile, (content1 + content2));
 }
 concatFiles(file1, file2, destFile);
