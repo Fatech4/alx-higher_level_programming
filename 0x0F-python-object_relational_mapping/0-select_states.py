9fafb6dcf134d30705452f4a6e64806c6425d6de
Faruk <adekoladamilola4@gmail.com>
2023-04-20 11:51:48 +0100

    Done with task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A module that connect to MySQL database and list all the states from the
+hbtn_0e_0_usa database """
+
+import MySQLdb
+
+db = MySQLdb.connect(
+        host='localhost', user='root', passwd='', db='hbtn_0e_0_usa'
+        )
+cur = db.cursor()
+cur.execute('SELECT * FROM states ORDER BY id ASC')
+for elements in cur.fetchall():
+    print(elements)
