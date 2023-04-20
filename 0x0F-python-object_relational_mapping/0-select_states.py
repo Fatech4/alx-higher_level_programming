915c2a24e58f4103465a7a0804df48e2ae37bfda
Faruk <adekoladamilola4@gmail.com>
2023-04-20 21:11:54 +0100

    Refix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,41 @@
 #!/usr/bin/python3
 """A module that connect to MySQL database and list all the states from the
 hbtn_0e_0_usa database"""
-if __name__ == '__main__':
-    import MySQLdb
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # check the number of arguments
+    if len(sys.argv) != 4:
+        print(
+            "Usage: {} <mysql username> <mysql password> <database name>".
+            format(sys.argv[0])
+            )
+        exit(1)
+
+    # connect to the database
     try:
         db = MySQLdb.connect(
-            host='localhost', user='root', passwd='', db='hbtn_0e_0_usa'
-         )
-        cur = db.cursor()
-        cur.execute('SELECT * FROM states ORDER BY id ASC')
-        for elements in cur.fetchall():
-            print(elements)
-    except Error:
-        pass
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL database: {}".format(e))
+        exit(1)
+
+    # execute the query to get all the states
+    try:
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except MySQLdb.Error as e:
+        print("Error executing MySQL query: {}".format(e))
+        exit(1)
+
+    # close the database connection
     db.close()
