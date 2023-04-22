d62bd54514996db45a9389b4872b5e1a1eab645b
Faruk <adekoladamilola4@gmail.com>
2023-04-22 12:39:37 +0100

    Refix 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" e a python file that contains the class definition of a State and an
+
+""" A python file that contains the class definition of a State and an
 instance Base = declarative_base() """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,6 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ A class the define the ORM for the states table"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
