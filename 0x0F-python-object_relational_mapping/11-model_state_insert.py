fc482d979e0e5974c5ed45335b54c646723a6aef
Faruk <adekoladamilola4@gmail.com>
2023-04-22 18:47:10 +0100

    Refix task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" A script that prints the first State object from the database
-hbtn_0e_6_usa """
+""" A script that adds the State object “Louisiana” to the database
+hbtn_0e_6_usa"""
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -16,3 +16,4 @@ if __name__ == "__main__":
     new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()
+    print(new_state.id)
