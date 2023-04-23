d16135186b9f5dbfde5f554dbbac7d8ab2c68d14
Faruk <adekoladamilola4@gmail.com>
2023-04-23 23:16:54 +0100

    Refix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,4 +15,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='state')
+    #cities = relationship('City', back_populates='state')
