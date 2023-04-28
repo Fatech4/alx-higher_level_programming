b31d7d9f27a4a3e609c6b6430f13bc3b9ae8b977
Faruk <adekoladamilola4@gmail.com>
2023-04-28 22:42:26 +0100

    Refix 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" A module that contains a function that finds a peak in a list of
+unsorted integers."""
+
+
 def find_peak(list_of_integers):
+    """ A function that finds a peak in a list of unsorted integers."""
     n = len(list_of_integers)
     if n == 1:
         return list_of_integers[0]
