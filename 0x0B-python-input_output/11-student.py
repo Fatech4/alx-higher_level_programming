3efc7e0f2deec0a6778ca6ac857107ad5ee38865
Faruk <adekoladamilola4@gmail.com>
2023-06-23 13:03:06 +0100

    fixes

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -18,7 +18,20 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Retrieve dict representation of instance
         """
+        valid_key = []
+        return_dict = {}
+        if type(attrs) is list:
+            for item in attrs:
+                if type(item) is str and item in self.__dict__:
+                    return_dict.update({item: self.__dict__[item]})
+            return return_dict
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace class attributes with those provided in json
+        """
+        if len(json) != 0:
+            self.__dict__ = json
